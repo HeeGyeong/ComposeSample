f7c3cd56b748418a1029bfc5e337fbedb5772618
HeeGyeong <fhskf94kr@naver.com>
2024-05-01 17:59:32 +0900

    refactor: sideEffect example 관련 설명 추가

--- app/src/main/java/com/example/composesample/example/ui/effect/SideEffectExampleUI.kt
+++ app/src/main/java/com/example/composesample/example/ui/effect/SideEffectExampleUI.kt
@@ -37,7 +37,6 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
@@ -52,7 +51,7 @@ fun SideEffectExampleUI(
 ) {
     val coroutineScope = rememberCoroutineScope()
 
-    // TextField Data 변경 시 마다 해당 데이터 변경하도록 설정.
+    /** TextField Data 변경 시 마다 해당 데이터 변경하도록 설정. */
     val isChanged = remember { mutableStateOf(false) }
 
     LazyColumn(
@@ -79,12 +78,9 @@ fun SideEffectExampleUI(
         }
 
         item {
+            /** Key 값을 Unit으로 하면, 생성 시 최초 한번만 호출된다. */
             LaunchedEffect(key1 = Unit, block = {
                 Log.d("SideEffectLog", "LaunchedEffect Unit - 생성 시 최초 한번 호출.")
-            })
-
-            LaunchedEffect(key1 = isChanged.value, block = {
-                Log.d("SideEffectLog", "LaunchedEffect isChanged.value - isChanged.value 변경 시 마다 호출.")
             })
 
             DisposableEffect(key1 = Unit, effect = {
@@ -95,15 +91,32 @@ fun SideEffectExampleUI(
                 }
             })
 
+            /** Key 값이 변경될 때 마다 호출 된다. */
+            LaunchedEffect(key1 = isChanged.value, block = {
+                Log.d(
+                    "SideEffectLog",
+                    "LaunchedEffect isChanged.value - isChanged.value 변경 시 마다 호출."
+                )
+            })
+
             DisposableEffect(key1 = isChanged.value, effect = {
-                Log.d("SideEffectLog", "DisposableEffect isChanged.value - isChanged.value[${isChanged.value}] 변경 시 마다 호출.")
+                Log.d(
+                    "SideEffectLog",
+                    "DisposableEffect isChanged.value - isChanged.value[${isChanged.value}] 변경 시 마다 호출."
+                )
 
                 onDispose {
-                    Log.d("SideEffectLog", "DisposableEffect isChanged.value onDispose - isChanged.value[${isChanged.value}] 변경 시 마다 호출.")
+                    Log.d(
+                        "SideEffectLog",
+                        "DisposableEffect isChanged.value onDispose - isChanged.value[${isChanged.value}] 변경 시 마다 호출."
+                    )
                 }
             })
 
-            // recomposable이 발생하지 않으면, 해당 부분도 최초 1회 호출하고 호출되지 않음.
+            /**
+             * 화면이 그려질 때 마다 호출 된다.
+             * recomposable이 발생하지 않으면, 최초 1회 호출하고 호출되지 않는다.
+             */
             SideEffect {
                 Log.d("SideEffectLog", "SideEffect - reComposable이 성공적으로 완료될 때 마다 호출")
             }
@@ -113,10 +126,15 @@ fun SideEffectExampleUI(
                 isChanged = isChanged
             )
 
-            // produceState
             var isTimer by remember { mutableStateOf(false) }
 
-            val timer by produceState(initialValue = 0, key1 = isTimer) {
+            /**
+             * produceState 는 key값이 변화할 때 마다 producer block을 실행하며, 해당 block에서 value 값을 변경하여 저장한다.
+             *
+             * 즉, 아래의 produceState에서 value 값은 initialValue를 0으로 설정했으므로 int Type으로 사용이 되며,
+             * isTimer 가 true일 때 시간에 따라 1씩 증가하도록 되어있다.
+             */
+            val timer by produceState(initialValue = 0, key1 = isTimer, producer = {
                 // 해당 블럭에서 Flow 등의 작업을 처리 후 initialValue에서 설정한 타입의 데이터의 값을 갱신한다.
                 var job: Job? = null
                 Log.d("SideEffectLog", "produceState .. $value") // value : Int Type.
@@ -129,16 +147,15 @@ fun SideEffectExampleUI(
                     }
                 }
 
-                // 종료 시 호출
+                // produceState 종료 시 호출, key 값이 변경될 때 마다 이전에 호출된 produceState가 있으면 종료 후 시작한다.
                 awaitDispose {
-                    // key 값이 변경될 때 마다 이전에 호출된 produceState가 있으면 종료 후 시작한다.
                     Log.d("SideEffectLog", "produceState awaitDispose .. $value")
                     job?.cancel()
                 }
-            }
+            })
 
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Text("Time $timer")
+                Text("Time : $timer")
                 Button(onClick = {
                     isTimer = !isTimer
                 }) {
@@ -146,9 +163,20 @@ fun SideEffectExampleUI(
                 }
             }
 
-            // derivedStateOf Example
+            /**
+             * derivedStateOf 는 block에서 계산된 결과를 반환한다.
+             *
+             * 특정 값이 변경될 때마다 데이터를 변경하는 것이 아닌 추가적인 계산을 해야할 때 사용한다.
+             */
             val derivedStateCheckDataBy by remember(timer) {
-                derivedStateOf { timer > 0 && timer % 2 == 0 }
+                derivedStateOf {
+                    if (timer == 0) {
+                        99999
+                    } else {
+                        Log.d("SideEffectLog", "derivedStateOf .. $timer")
+                        timer / 5
+                    }
+                }
             }
 
             val derivedStateCheckData = remember(timer) {
@@ -158,11 +186,16 @@ fun SideEffectExampleUI(
             Text(text = "derivedStateCheckData : $derivedStateCheckDataBy")
             Text(text = "derivedStateCheckData2 : ${derivedStateCheckData.value}")
 
-            // snapshotFlow Example
-            LaunchedEffect(key1 = isChanged, block = {
+            /**
+             * snapshotFlow 를 통해 isChanged.value 의 상태를 관찰한다.
+             *
+             * Unit으로 선언하여 최초 한번 실행되지만, 명시적으로 수집을 취소하지 않거나 다른 Flow를 사용하지 않으면,
+             * 무기한 계속 관찰한다.
+             */
+            LaunchedEffect(key1 = Unit, block = {
                 snapshotFlow { isChanged.value } // isChanged 중 isChange의 Value가 변경되었을 때 호출.
                     .map { timer > 0 && timer % 2 != 0 } // 호출 조건
-                    .distinctUntilChanged() // 중복 제거
+                    .distinctUntilChanged() // 이전의 값과 동일한 값이면 건너뛴다.
                     .collect {
                         Log.d("SideEffectLog", "snapshotFlow $it")
                     }
@@ -171,11 +204,13 @@ fun SideEffectExampleUI(
     }
 }
 
+/** remember 관련 sideEffect Example */
 @Composable
 fun UpdateRememberExample(
     isChanged: MutableState<Boolean>
 ) {
     var textState by remember { mutableStateOf("Default") }
+//    val textState = remember { mutableStateOf("Default") }
 
     Column(modifier = Modifier.padding(16.dp)) {
         TextField(
@@ -204,14 +239,14 @@ fun UpdateRememberExample(
 @Composable
 fun RememberUpdateTestText(text: String) {
     val rememberTextBy by remember { mutableStateOf(text) }
+    val rememberText = remember { mutableStateOf(text) }
+
+    /** rememberUpdatedState를 확인 해보면 rememberTextByApply 와 동일한 것을 알 수 있다. */
+    val rememberUpdatedText by rememberUpdatedState(text)
     val rememberTextByApply by remember { mutableStateOf(text) }
         .apply {
             value = text
         }
-    val rememberText = remember { mutableStateOf(text) }
-
-    // rememberUpdatedState를 확인 해보면 rememberTextByApply 와 동일한 것을 알 수 있다.
-    val rememberUpdatedText by rememberUpdatedState(text)
 
     Text("rememberTextBy : $rememberTextBy")
     Text("rememberText : ${rememberText.value}")
