dba87a478ae94ed9949d046224e76e0a93728693
HeeGyeong <fhskf94kr@naver.com>
2024-06-01 19:09:30 +0900

    chore: 코드 컨벤션 정리

--- app/src/main/java/com/example/composesample/example/ui/api/ApiDisconnectExampleUI.kt
+++ app/src/main/java/com/example/composesample/example/ui/api/ApiDisconnectExampleUI.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -31,7 +29,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import com.example.composesample.example.util.NetworkUtil
 import org.koin.androidx.compose.get
@@ -98,7 +95,8 @@ fun ApiDisconnectExampleUI(
 
                     if (!isConnectNetwork.value) {
                         Column(
-                            modifier = Modifier.fillMaxWidth()
+                            modifier = Modifier
+                                .fillMaxWidth()
                                 .background(color = Color.Black),
                             horizontalAlignment = Alignment.CenterHorizontally,
                             verticalArrangement = Arrangement.Center
