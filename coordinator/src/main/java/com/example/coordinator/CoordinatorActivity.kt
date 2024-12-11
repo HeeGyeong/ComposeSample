c7cfad32519aa83792e0c30030f799666eb49d94
HeeGyeong <fhskf94kr@naver.com>
2024-12-11 23:47:27 +0900

    refactor: coordinator pattern flow 주석 추가

--- coordinator/src/main/java/com/example/coordinator/CoordinatorActivity.kt
+++ coordinator/src/main/java/com/example/coordinator/CoordinatorActivity.kt
@@ -12,7 +12,21 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import org.koin.androidx.compose.koinViewModel
 
-
+/**
+ * Coordinator pattern Flow
+ *
+ * 1. Core Module - Navigation Interface, Navigation Interface를 매개변수로 가지는 Class(A)생성
+ * 2. Core Module - A를 매개변수로 가지는 BaseViewModel 생성 : 모든 ViewModel이 상속받아서 사용, ViewModel에서 화면전환을 위함
+ * 3. 각 Module - ViewModel에서 BaseViewModel을 상속 받아서 사용 : 상속을 위해 모든 viewModel이 Class A를 매개 변수로 가짐.
+ * 4. App Module - Navigation Interface의 구현체 NavigationImpl 생성 (Coordinator) : 실제 화면 변경을 위한 함수 호출하는 부분
+ * 5. 각 viewModel - baseViewModel에 선언한 class A 함수를 호출 -> NavigationImpl(Coordinator)의 changeActivity 호출
+ * 6. NavigationImpl(Coordinator)의 changeActivity에서는 각 Module에 선언 되어있는 Activity 변경을 수행하는 함수(Func B)를 호출.
+ * 7. 각 Module에서 선언된 Func B를 통해 Activity 전환이 일어남.
+ *
+ * B Module Activity -> BViewModel을 통해 changeActivity 호출
+ * -> 구현체인 App Module의 NavigationImpl(Coordinator)의 changeActivity 호출
+ * -> 각 Module의 Func B 호출 -> 실제 화면 전환
+ */
 class CoordinatorActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
