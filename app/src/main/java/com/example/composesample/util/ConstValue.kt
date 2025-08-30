8fa0e6c7cf353a85946934687b5fae43ca591241
HeeGyeong <fhskf94kr@naver.com>
2025-08-30 12:35:56 +0900

    chore: update date update

--- app/src/main/java/com/example/composesample/util/ConstValue.kt
+++ app/src/main/java/com/example/composesample/util/ConstValue.kt
@@ -3,7 +3,7 @@ package com.example.composesample.util
 class ConstValue {
     companion object {
         // Update date
-        const val UpdateDate = "25년 8월"
+        const val UpdateDate = "25년 9월"
 
         // Code Type
         const val IntentType = "type"
